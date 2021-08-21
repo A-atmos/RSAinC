2cfe4dd813076e9a253ed23333c9ce1af1d74464
Kushal576 <impk576@gmail.com>
2021-08-21 19:50:11 +0545

    Save array and read as array from a file

---
+++ file.h
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int save (int a[99])
+{
+    FILE *file;
+        int i;
+        char name[99];
+        printf("Name of file:");
+        scanf("%s",name);
+        file=fopen(name,"w");
+        if(file==NULL)
+            printf("No file found");
+        for (i=0;a[i]!='\0';i++)
+            fprintf(file,"%d ",a[i]);
+    fprintf(file,"\0");
+    return 0;
+}
+int read(int a[99])
+{
+    FILE *file;
+    int i=0;
+    char fn[99];
+    printf("Enter the name of file to open:");
+    scanf("%s",fn);
+    file=fopen("text4.txt","r");
+    if(fopen==NULL)
+        exit(0);
+    for (i=0;a[i]!=EOF;i++)
+    {
+    fscanf(file," %d",&a[i]);
+    }
+    a[i]=EOF;
+    return 0;
+}
